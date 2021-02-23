8ab174b7a7cef54d7930395f5bd0bbdf65f4e954
Sleepmin <tickiock08@gmail.com>
2021-02-24 01:05:53 +0900

    update

--- src/main/java/com/github/highright1234/excommand/Commands.java
+++ src/main/java/com/github/highright1234/excommand/Commands.java
@@ -1,39 +1,53 @@
 package com.github.highright1234.excommand;
 
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.arguments.Argument;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-
+import static com.github.highright1234.excommand.Functions.writeAndSave;
 import static com.github.highright1234.excommand.Vars.jsonObject;
-public class Commands implements CommandExecutor {
 
+public class Commands {
 
-    @Override
+    private void printHelp(CommandSender sender,String helpType) {
+        if (helpType == null) {
+            sender.sendMessage("asdf");
+        } else if (helpType.equals("reload")) {
+
+        }
+    }
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         Scoreboard board = Objects.requireNonNull(manager).getNewScoreboard();
         Objective objective = board.registerNewObjective(args[0], args[1], args[2]);
-        
-        jsonObject.addProperty("name", "makesomething");
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("sample.txt"), 1);
-            bufferedWriter.write(jsonObject.toString());
-            bufferedWriter.flush();
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        return false;
+        writeAndSave("name", args[3]);
+
+        return true;
+    }
+    public static void commands() {
+        List<Argument> arguments = new ArrayList<>();
+        arguments.add(new GreedyStringArgument("message"));
+        new CommandAPICommand("broadcastmsg")
+                .withArguments(arguments)                     // The arguments
+                .withAliases("broadcast", "broadcastmessage") // Command aliases
+                .withPermission(CommandPermission.OP)         // Required permissions
+                .executes((sender, args) -> {
+                    String message = (String) args[0];
+                    Bukkit.getServer().broadcastMessage(message);
+                }).register();
     }
 }
