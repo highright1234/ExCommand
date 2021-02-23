fa62c40fdf69dd02eec9b7a75853d768f3b69cce
Sleepmin <tickiock08@gmail.com>
2021-02-23 20:46:38 +0900

    Initial commit

---
+++ src/main/java/com/github/highright1234/excommand/ExCommand.java
@@ -0,0 +1,17 @@
+package com.github.highright1234.excommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExCommand extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
