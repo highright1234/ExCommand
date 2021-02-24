cbeca26d7551fa3332b06f8971efa2717f49d9aa
Sleepmin <tickiock08@gmail.com>
2021-02-24 14:43:41 +0900

    update

--- src/main/java/com/github/highright1234/excommand/Commands.java
+++ src/main/java/com/github/highright1234/excommand/Commands.java
@@ -6,6 +6,7 @@ import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -18,7 +19,7 @@ import java.util.Objects;
 import static com.github.highright1234.excommand.Functions.writeAndSave;
 import static com.github.highright1234.excommand.Vars.jsonObject;
 
-public class Commands {
+public class Commands implements CommandExecutor {
 
     private void printHelp(CommandSender sender,String helpType) {
         if (helpType == null) {
@@ -37,17 +38,5 @@ public class Commands {
         writeAndSave("name", args[3]);
 
         return true;
-    }
-    public static void commands() {
-        List<Argument> arguments = new ArrayList<>();
-        arguments.add(new GreedyStringArgument("message"));
-        new CommandAPICommand("broadcastmsg")
-                .withArguments(arguments)                     // The arguments
-                .withAliases("broadcast", "broadcastmessage") // Command aliases
-                .withPermission(CommandPermission.OP)         // Required permissions
-                .executes((sender, args) -> {
-                    String message = (String) args[0];
-                    Bukkit.getServer().broadcastMessage(message);
-                }).register();
     }
 }
--- src/main/java/com/github/highright1234/excommand/ExCommand.java
+++ src/main/java/com/github/highright1234/excommand/ExCommand.java
@@ -1,37 +1,14 @@
 package com.github.highright1234.excommand;
 
-import dev.jorel.commandapi.CommandAPI;
-import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.Argument;
-import dev.jorel.commandapi.arguments.GreedyStringArgument;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.github.highright1234.excommand.Commands.commands;
+import java.util.Objects;
 
 public final class ExCommand extends JavaPlugin {
 
     @Override
-    public void onLoad() {
-        CommandAPI.onLoad(true); //Load with verbose output
-        commands();
-        List<Argument> arguments = new ArrayList<>();
-        arguments.add(new GreedyStringArgument("message"));
-        new CommandAPICommand("broadcastmsg")
-                .withArguments(arguments)                     // The arguments
-                .withAliases("broadcast", "broadcastmessage") // Command aliases
-                .executes((sender, args) -> {
-                    String message = (String) args[0];
-                    Bukkit.getServer().broadcastMessage(message);
-                }).register();
-    }
-
-    @Override
     public void onEnable() {
-        CommandAPI.onEnable(this);
+        Objects.requireNonNull(getCommand("excommand")).setExecutor(new Commands());
         // Plugin startup logic
 
     }
