c05e371a840b3289633043b1f8781953a3823a6d
Sleepmin <tickiock08@gmail.com>
2021-02-25 18:08:40 +0900

    update

--- src/main/java/com/github/highright1234/excommand/EventsManager.java
+++ src/main/java/com/github/highright1234/excommand/EventsManager.java
@@ -1,8 +1,10 @@
 package com.github.highright1234.excommand;
 
 public class EventsManager {
-    public static void 
     public void eventsSetup() {
         // TODO add a events setup
+    }
+    public static void eventMethod() {
+        // TODO
     }
 }
